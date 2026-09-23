ca8eddad0ea787b41f38a56c97ce699d0c490656
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-09-23 12:28:40 +0000

    Deploying to gh-pages from @ b-on-g/WikiLive@22e49c942b8579c081f5f16eb44d0bb062584adc 🚀

--- node.test.js
+++ node.test.js
@@ -16178,6 +16178,8 @@ var $;
             for (const gift of this._gift.values()) {
                 if (mine.units_persisted.has(gift))
                     continue;
+                if (!this.unit_seal(gift))
+                    continue;
                 persisting.add(gift);
                 check_lord(gift.lord());
                 check_lord(gift.mate());
@@ -16186,6 +16188,8 @@ var $;
                 for (const units of kids.values()) {
                     for (const sand of units.values()) {
                         if ($mol_wire_sync(mine.units_persisted).has(sand))
+                            continue;
+                        if (!this.unit_seal(sand))
                             continue;
                         persisting.add(sand);
                         check_lord(sand.lord());
